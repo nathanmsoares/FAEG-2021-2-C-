17b6aed03a398da584c4ebdadcde1f2e6c02d2ec
Nathan Mainka Soares <50429336+nathanmsoares@users.noreply.github.com>
2021-09-28 17:42:29 -0300

    Last week class

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Curso_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste");
+        }
+    }
+}
