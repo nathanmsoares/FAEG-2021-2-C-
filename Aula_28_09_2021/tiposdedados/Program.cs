95debe6ac10f44dbc52ba07faa38d2b2097d5096
Nathan Mainka Soares <50429336+nathanmsoares@users.noreply.github.com>
2021-09-28 19:12:21 -0300

    09-28-2021 C# class

---
+++ Aula_28_09_2021/tiposdedados/Program.cs
@@ -0,0 +1,25 @@
+﻿// Importar bibliotecas externas (import)
+using System;
+
+// Agrupador de classes (package)
+namespace tiposdedados
+{
+    // Classe - junção MÉTODOS e ATRIBUTOS/PROPRIEDADES
+    //static - não modifica - carregador para memória antes do inicio do programa
+    class Program
+    {
+        //MÉTODO - 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Tipo Primitivo
+            int idade = 10;
+            string nome = "Zezinho";
+            Console.WriteLine("-Nome: " + nome + " Idade: " +idade); // concatenação
+            // Interpolação
+            Console.WriteLine($"Nome: {nome} Idade: {idade}"); // System.out.println("")
+        }
+        // AQUI OHHH
+    }
+}
